d26d3420bd422fb6f7cff40eb2406a2f9a978422
angel <angelos2250@hotmail.com>
2023-11-05 18:50:42 +0100

    fix some eslint errors

--- backend/src/rooms/schema/MyRoomState.ts
+++ backend/src/rooms/schema/MyRoomState.ts
@@ -1,7 +1,7 @@
-import { Schema, Context, type } from "@colyseus/schema";
+import { Schema, Context, type } from '@colyseus/schema';
 
 export class MyRoomState extends Schema {
 
-  @type("string") mySynchronizedProperty: string = "Hello world";
+  @type('string') mySynchronizedProperty: string = 'Hello world';
 
 }
