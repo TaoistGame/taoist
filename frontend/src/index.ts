91bfc357ff184d8184b44fb6ee564ce609fff133
Berhan <bero1906@outlook.de>
2023-11-07 22:50:54 +0100

    Added a Text Button, which prints 'Button clicked!' in the website console when clicked

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -1,28 +1,39 @@
 import Phaser from 'phaser';
 import logoImg from './assets/logo.png';
 
+
+
 class MyGame extends Phaser.Scene
 {
+
+  button: Phaser.GameObjects.Text | undefined;
   constructor ()
   {
-    super();
+    super({ key: 'GameScene' });
   }
 
   preload ()
   {
+    this.load.image('button','assets/thad.jpg');
     //  This is an example of a bundled image:
     this.load.image('logo', logoImg);
     //  This is an example of loading a static image from the public folder:
-    //this.load.image('background', 'assets/bg.jpg');
-    this.load.image('background', 'assets/background.jpg');
+    this.load.image('background', 'assets/bg.jpg');
+    //this.load.image('background', 'assets/background.jpg');
   }
       
   create ()
   {
+
     this.add.image(400, 300, 'background');
 
     const logo = this.add.image(400, 150, 'logo');
-      
+
+    this.button= this.add.text(200,200,'Click me!')
+        .setInteractive().on('pointerdown',()=>this.onButtonClick());
+
+
+
     this.tweens.add({
       targets: logo,
       y: 450,
@@ -31,6 +42,10 @@ class MyGame extends Phaser.Scene
       yoyo: true,
       loop: -1
     });
+  }
+
+  private onButtonClick() {
+    console.log('Button clicked');
   }
 }
 
