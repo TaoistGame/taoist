b622348f73329060db4d82d1351b720e22ac10ac
Gamefrend <123830166+Gamefrend@users.noreply.github.com>
2023-11-09 14:16:27 +0100

    Bisschen mit free assets add image und physics gravety rum probiert

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -1,6 +1,10 @@
 import Phaser from 'phaser';
 import logoImg from './assets/logo.png';
+import background from './assets/TX Tileset Grass.png';
+import character from './assets/characters/player.png';
 
+const gameWidth:number = 800;
+const gameHeight:number = 600;
 class MyGame extends Phaser.Scene
 {
   constructor ()
@@ -13,15 +17,23 @@ class MyGame extends Phaser.Scene
     //  This is an example of a bundled image:
     this.load.image('logo', logoImg);
     //  This is an example of loading a static image from the public folder:
-    this.load.image('background', 'assets/bg.jpg');
+    this.load.image('background', background);
+    this.load.image('character', character);
   }
       
   create ()
   {
+    this.anims.create({
+      key: 'left',
+      frames: this.anims.generateFrameNumbers('character',{ start:3,end:3 }),
+      frameRate: 10,
+      repeat: -1
+    });
     this.add.image(400, 300, 'background');
-
     const logo = this.add.image(400, 150, 'logo');
-      
+    const player = this.physics.add.sprite(100, 450, 'character');
+    player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
     this.tweens.add({
       targets: logo,
       y: 450,
@@ -36,9 +48,16 @@ class MyGame extends Phaser.Scene
 const config = {
   type: Phaser.AUTO,
   parent: 'phaser-example',
-  width: 800,
-  height: 600,
-  scene: MyGame
+  width: gameWidth,
+  height: gameHeight,
+  scene: MyGame,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 10 },
+      debug: false
+    }
+  }
 };
 
 const game = new Phaser.Game(config);
