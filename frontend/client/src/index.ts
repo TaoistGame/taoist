43342b03ef8216d48be3aeded5422451b463158a
angel <angelos2250@hotmail.com>
2024-03-03 15:49:53 +0100

    extend base init

---
+++ frontend/client/src/index.ts
@@ -0,0 +1,26 @@
+import Phaser from "phaser";
+
+import { StarterVillageScene } from "./scenes/StarterVillageScene";
+
+import { BACKEND_HTTP_URL } from "./backend";
+
+const config: Phaser.Types.Core.GameConfig = {
+    type: Phaser.AUTO,
+    fps: {
+        target: 60,
+        forceSetTimeOut: true,
+        smoothStep: false,
+    },
+    width: 800,
+    height: 600,
+    // height: 200,
+    backgroundColor: '#b6d53c',
+    parent: 'phaser-example',
+    physics: {
+        default: "arcade"
+    },
+    pixelArt: true,
+    scene: [StarterVillageScene],
+};
+
+const game = new Phaser.Game(config);
